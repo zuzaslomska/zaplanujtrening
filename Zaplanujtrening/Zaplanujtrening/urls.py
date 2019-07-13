a165f3aa275e663a8e6be7aae81d00f59a73782e
Tomaszdud <tomasz.dudek1997@gmail.com>
2019-07-13 12:59:31 +0200

    preparing settings

--- Zaplanujtrening/Zaplanujtrening/urls.py
+++ Zaplanujtrening/Zaplanujtrening/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from Trening.views import MainSite
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', MainSite.as_view()),
 ]
